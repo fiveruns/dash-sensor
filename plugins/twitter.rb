3f0fa738fc6e62a81c2daac5e08503bf6c06aa0c
Mike Perham <mperham@gmail.com>
2009-02-19 15:07:02 -0600

    Cache to avoid the twitter police

--- plugins/twitter.rb
+++ plugins/twitter.rb
@@ -42,7 +42,7 @@ module Dash::Sensor::Plugins
 
     def self.data
       # Cache the data for 15 minutes, we don't really need it every minute.
-      if !@time || @time < Time.now - 90
+      if !@time || @time < Time.now - 900
         logger.debug "Fetching data at #{Time.now}"
         @old_data = @data || Hash.new(1_000_000_000)
         @data = instance.send(:data)
